75826736ffdc40d5cc09437fd26b750c16191d57
dev <68017625+TechSavage2@users.noreply.github.com>
2024-01-06 16:28:15 -0600

    Demo updated with tag in test text

--- demos/post-a-test-message.js
+++ demos/post-a-test-message.js
@@ -18,9 +18,11 @@ import { postMessage } from '../src/api.auth.statuses.js';
   const lo = await login();
 
   // post a message
-  const result = await postMessage(lo, '### This message was posted with gablib\n', {
-    visibility: enumVisibility.private
-  });
+  const result = await postMessage(lo,
+    '### This message was posted with gablib\n\n#gablib **rocks!** Find gablib here:\n\nhttps://github.com/TechSavage2/gablib',
+    {
+      visibility: enumVisibility.public
+    });
 
   console.log(result);
 
