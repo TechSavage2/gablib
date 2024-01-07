bc902b4190d612f2a10f2cf1f84def19a2ed097e
dev <68017625+TechSavage2@users.noreply.github.com>
2024-01-07 05:52:48 -0600

    Added (un)favoriting posts incl. various reaction types

--- src/api.auth.statuses.js
+++ src/api.auth.statuses.js
@@ -133,6 +133,21 @@ export async function getTimelineStatuses(lo, timeline = 'home', pageOrMaxId = 0
   return await _getStatuses(lo, url);
 }
 
+export async function favoritePost(lo, statusId, reactId = '1') {
+  const url = lo.baseUrl + `/api/v1/statuses/${ statusId }/favourite`;
+  const body = {};
+  if ( reactId !== '1' ) body.reaction_id = reactId;
+  const result = await _fetch(lo, url, 'POST', 'json', body);
+  return { content: result.content, ok: result.status === 200 };
+}
+
+export async function unfavoritePost(lo, statusId, reactId = '1') {
+  const url = lo.baseUrl + `/api/v1/statuses/${ statusId }/unfavourite`;
+  const body = {};
+  const result = await _fetch(lo, url, 'POST', 'json', body);
+  return { content: result.content, ok: result.status === 200 };
+}
+
 /*******************************************************************************
 
  HELPER FUNCTIONS
