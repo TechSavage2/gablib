c82b854149c86cda23eb5f10a98a734903c14017
dev <68017625+TechSavage2@users.noreply.github.com>
2024-01-09 15:40:21 -0600

    Updated barrel file with accounts module

--- gablib.js
+++ gablib.js
@@ -11,6 +11,7 @@
 export * from './src/login.js';
 export * from './src/api.public.js';
 export * from './src/api.auth.js';
+export * from './src/api.auth.accounts.js';
 export * from './src/api.auth.bookmarks.js';
 export * from './src/api.auth.groups.js';
 export * from './src/api.auth.statuses.js';
