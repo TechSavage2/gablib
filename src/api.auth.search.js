9905cbd138c280bcea3d87357ae426f2caa18ab9
dev <68017625+TechSavage2@users.noreply.github.com>
2024-01-09 07:37:04 -0600

    Added full support site wide search incl. filters, paginating

---
+++ src/api.auth.search.js
@@ -0,0 +1,55 @@
+/*******************************************************************************
+
+ gablib
+ api.auth.search.js (2024-01-09)
+ Copyright (c) 2024 TechSavage
+
+ *******************************************************************************/
+
+/**
+ * @module Search
+ */
+
+'use strict';
+
+import { _fetch } from './_fetch.js';
+
+/**
+ * Search site for query. Filter type must be set to one type, defaults to 'status'.
+ * @param {LoginObject} lo - Valid and active LoginObject
+ * @param {string} query Search query, keywords
+ * @param {number|string} [page=1] page number for results
+ * @param {Object} [filters={}] filters for query
+ * @param {boolean} [filters.onlyVerified] only show results from verified accounts
+ * @param {string} [filters.type='status'] filter results on type: See {@link enumSearchFilterTypes}.
+ * 'status', 'group', 'top', 'account', 'link', 'feed', 'hashtag'
+ * @returns {Promise<*>}
+ * @throws If query is empty
+ */
+export async function search(lo, query, page = 1, filters = {}) {
+    filters = Object.assign({}, {
+      onlyVerified: false,
+      type: 'status'
+    }, filters)
+
+  const url = new URL('/api/v3/search', lo.baseUrl);
+
+  if ( typeof query !== 'string' || !query.trim().length ) {
+    throw new Error('Search query is empty.');
+  }
+
+  url.searchParams.append('q', query);
+  url.searchParams.append('resolve', 'true');
+  url.searchParams.append('onlyVerified', (!!filters.onlyVerified).toString());
+
+  page |= 0;
+  if ( page > 0 ) {
+    url.searchParams.append('page', page);
+  }
+
+  if ( typeof filters.type === 'string' ) {
+    url.searchParams.append('type', filters.type);
+  }
+
+  return _fetch(lo, url);
+}
