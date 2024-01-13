23eb34650afc656cc5afa81ea1aa9a502c69434f
dev <68017625+TechSavage2@users.noreply.github.com>
2024-01-13 10:30:06 -0600

    CLN Sorted enums.

--- src/enums.js
+++ src/enums.js
@@ -9,24 +9,63 @@
 'use strict';
 
 /**
- * Sorting methods for when requesting timelines.
+ * Enum for handling account follow requests.
  * @global
  * @enum {string}
  * @readonly
  */
-export const enumStatusSort = {
-  newest          : 'newest',
-  newestWithGroups: 'with-groups',
-  newestNoReposts : 'no-reposts',
-  hot             : 'hot',
-  top             : 'top',
-  recent          : 'recent', // pro??
-  rising          : 'rising', // does not work with pro, does with explore
-  topToday        : 'top_today', // pro??
-  topWeekly       : 'top_weekly', // pro??
-  topMonthly      : 'top_monthly', // pro??
-  topYearly       : 'top_yearly', // pro??
-  topAllTime      : 'top_all_time' // pro??
+export const enumAccountFollowRequest = {
+  approve: 'authorize',
+  reject : 'reject'
+};
+
+/**
+ * Valid media types when requesting media attachments from account timelines.
+ * @global
+ * @enum {string}
+ * @readonly
+ */
+export const enumAccountMediaType = {
+  image: 'image',
+  video: 'video'
+};
+
+/**
+ * Valid feed types.
+ * @global
+ * @enum {string}
+ * @readonly
+ */
+export const enumFeedType = {
+  own     : 'own',
+  featured: 'featured',
+  memberOf: 'member_of'
+};
+
+/**
+ * Enum for handling group join requests.
+ * @global
+ * @enum {string}
+ * @readonly
+ */
+export const enumGroupModerationJoin = {
+  approve: 'approve',
+  reject : 'reject'
+};
+
+/**
+ * Expire times for new polls. You can use other values than these,
+ * but these are the defaults some sites are using.
+ * @global
+ * @enum {number}
+ * @readonly
+ */
+export const enumPollExpires = {
+  oneHour  : 3600,
+  sixHours : 21600,
+  oneDay   : 86400,
+  threeDays: 259200,
+  oneWeek  : 604800
 };
 
 /**
@@ -46,33 +85,6 @@ export const enumPostExpires = {
 };
 
 /**
- * Expire times for new polls. You can use other values than these,
- * but these are the defaults some sites are using.
- * @global
- * @enum {number}
- * @readonly
- */
-export const enumPollExpires = {
-  oneHour  : 3600,
-  sixHours : 21600,
-  oneDay   : 86400,
-  threeDays: 259200,
-  oneWeek  : 604800
-};
-
-/**
- * Visibility for new or edited statuses.
- * @global
- * @enum {string}
- * @readonly
- */
-export const enumVisibility = {
-  public  : 'public',
-  private : 'private',
-  unlisted: 'unlisted'
-};
-
-/**
  * Reaction IDs for when marking a status favorite.
  * @global
  * @enum {string}
@@ -87,6 +99,56 @@ export const enumReactions = {
   heart     : '6',
   salute    : '11',
   xmasTree  : '15'
+};
+
+/**
+ * Valid filters for global `search()`
+ * @global
+ * @enum {string}
+ * @readonly
+ */
+export const enumSearchFilterTypes = {
+  all    : null,
+  status : 'status',
+  group  : 'group',
+  top    : 'top',
+  account: 'account',
+  link   : 'link',
+  feed   : 'feed',
+  hashtag: 'hashtag'
+};
+
+/**
+ * Valid shortcut types.
+ * @global
+ * @enum {string}
+ * @readonly
+ */
+export const enumShortType = {
+  account: 'account',
+  group  : 'group',
+  list   : 'list'
+};
+
+/**
+ * Sorting methods for when requesting timelines.
+ * @global
+ * @enum {string}
+ * @readonly
+ */
+export const enumStatusSort = {
+  newest          : 'newest',
+  newestWithGroups: 'with-groups',
+  newestNoReposts : 'no-reposts',
+  hot             : 'hot',
+  top             : 'top',
+  recent          : 'recent', // pro??
+  rising          : 'rising', // does not work with pro, does with explore
+  topToday        : 'top_today', // pro??
+  topWeekly       : 'top_weekly', // pro??
+  topMonthly      : 'top_monthly', // pro??
+  topYearly       : 'top_yearly', // pro??
+  topAllTime      : 'top_all_time' // pro??
 };
 
 /**
@@ -110,75 +172,13 @@ export const enumTimelines = {
 };
 
 /**
- * Valid filters for global `search()`
+ * Visibility for new or edited statuses.
  * @global
  * @enum {string}
  * @readonly
  */
-export const enumSearchFilterTypes = {
-  all    : null,
-  status : 'status',
-  group  : 'group',
-  top    : 'top',
-  account: 'account',
-  link   : 'link',
-  feed   : 'feed',
-  hashtag: 'hashtag'
-};
-
-/**
- * Enum for handling group join requests.
- * @global
- * @enum {string}
- * @readonly
- */
-export const enumGroupModerationJoin = {
-  approve: 'approve',
-  reject : 'reject'
-};
-
-/**
- * Enum for handling account follow requests.
- * @global
- * @enum {string}
- * @readonly
- */
-export const enumAccountFollowRequest = {
-  approve: 'authorize',
-  reject : 'reject'
-};
-
-/**
- * Valid media types when requesting media attachments from account timelines.
- * @global
- * @enum {string}
- * @readonly
- */
-export const enumAccountMediaType = {
-  image: 'image',
-  video: 'video'
-};
-
-/**
- * Valid shortcut types.
- * @global
- * @enum {string}
- * @readonly
- */
-export const enumShortType = {
-  account: 'account',
-  group  : 'group',
-  list   : 'list'
-};
-
-/**
- * Valid feed types.
- * @global
- * @enum {string}
- * @readonly
- */
-export const enumFeedType = {
-  own     : 'own',
-  featured: 'featured',
-  memberOf: 'member_of'
+export const enumVisibility = {
+  public  : 'public',
+  private : 'private',
+  unlisted: 'unlisted'
 };
