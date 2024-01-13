64478cda9138d390b29ba0be6e8e374daf99f5f4
dev <68017625+TechSavage2@users.noreply.github.com>
2024-01-13 09:24:33 -0600

    DOC

--- src/obj/Poll.js
+++ src/obj/Poll.js
@@ -14,24 +14,40 @@ const MAX_OPTIONS = 8;
 
 /**
  * Poll helper object to create polls for postMessage statuses.
- * @param options
+ * The poll options will appear in the order they are given/added.
+ * @param {[]} [options=[]] optional array with string entries representing the options.
  * @constructor
  */
 export function Poll(options = []) {
   let expires = enumPollExpires.threeDays;
 
+  /**
+   * Add an option.
+   * @param {string} text Option text
+   * @returns {boolean} May fail if maximum number of options has been reached.
+   */
   this.add = function(text) {
     if ( options.length >= MAX_OPTIONS || typeof text !== 'string' ) return false;
     options.push(text);
     return true;
   };
 
-  this.setExpire = function(seconds) {
+  /**
+   * Set expire delta for the poll to expire. (See {@link enumPollExpires}).
+   * @param {number|enumPollExpires} [seconds=enumPollExpires.threeDays] - delta in number of seconds.
+   * @returns {boolean}
+   */
+  this.setExpire = function(seconds = enumPollExpires.threeDays) {
     if ( seconds < enumPollExpires.oneHour || seconds > enumPollExpires.oneWeek ) return false;
     expires = seconds | 0;
     return true;
   };
 
+  /**
+   * Produce the poll JSON object that is sent to server. This is handled
+   * automatically internally by the `createStatus()` call.
+   * @returns {{options: *[], expires_in: (enumPollExpires|string)}}
+   */
   this.toJSON = function() {
     return { options, expires_in: expires };
   };
