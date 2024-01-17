08cfc99a343056177d5e6b286dcee682d9386148
dev <68017625+TechSavage2@users.noreply.github.com>
2024-01-16 22:21:51 -0600

    UPD modules, rearranged

--- gablib.js
+++ gablib.js
@@ -9,16 +9,18 @@
 'use strict';
 
 export * from './src/login.js';
-export * from './src/api.public.js';
 export * from './src/api.auth.js';
 export * from './src/api.auth.accounts.js';
 export * from './src/api.auth.bookmarks.js';
 export * from './src/api.auth.directmessages.js';
 export * from './src/api.auth.feeds.js';
 export * from './src/api.auth.groups.js';
-export * from './src/api.auth.statuses.js';
 export * from './src/api.auth.notifications.js';
+export * from './src/api.auth.search.js';
 export * from './src/api.auth.shortcuts.js';
+export * from './src/api.auth.statuses.js';
 export * from './src/api.auth.stream.js';
-//export * from './src/api.auth.settings.js';
+export * from './src/api.public.js';
+
+export * from './src/enums.js';
 export * from './src/obj/Poll.js';
